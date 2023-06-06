b093f818d63f8da6a64114ce8ad2bcb636c40791
kawax <kawaxbiz@gmail.com>
2023-06-06 13:27:19 +0900

    wip

--- src/Contracts/Factory.php
+++ src/Contracts/Factory.php
@@ -7,98 +7,34 @@ use Psr\Http\Message\ResponseInterface;
 
 interface Factory
 {
-    /**
-     * @param  string  $client_name
-     * @param  string  $redirect_uris
-     * @param  string  $scopes
-     * @param  string  $website
-     * @return array
-     */
     public function createApp(string $client_name, string $redirect_uris, string $scopes, string $website = ''): array;
 
-    /**
-     * @return array
-     */
     public function verifyCredentials(): array;
 
-    /**
-     * @param  string  $method
-     * @param  string  $api
-     * @param  array  $options
-     * @return array
-     */
     public function call(string $method, string $api, array $options = []): array;
 
-    /**
-     * @param  string  $api
-     * @param  array  $query
-     * @return array
-     */
     public function get(string $api, array $query = []): array;
 
-    /**
-     * @param  string  $api
-     * @param  array  $params
-     * @return array
-     */
     public function post(string $api, array $params = []): array;
 
-    /**
-     * @return string
-     */
     public function apiEndpoint(): string;
 
-    /**
-     * @param  ClientInterface  $client
-     * @return Factory
-     */
     public function setClient(ClientInterface $client): static;
 
-    /**
-     * @param  string  $domain
-     * @return Factory
-     */
     public function domain(string $domain): static;
 
-    /**
-     * @param  string  $token
-     * @return Factory
-     */
     public function token(string $token): static;
 
-    /**
-     * @param  string  $api_version
-     * @return Factory
-     */
     public function apiVersion(string $api_version): static;
 
-    /**
-     * @param  string  $api_base
-     * @return Factory
-     */
     public function apiBase(string $api_base): static;
 
     public function getResponse(): ?ResponseInterface;
 
-    /**
-     * @param  int  $account_id
-     * @param  int  $limit
-     * @param  int  $since_id
-     * @return array
-     */
     public function statuses(int $account_id, int $limit = 40, int $since_id = null): array;
 
-    /**
-     * @param  string  $status
-     * @param  array  $options
-     * @return array
-     */
     public function createStatus(string $status, array $options = []): array;
 
-    /**
-     * @param  int  $status_id
-     * @return array
-     */
     public function status(int $status_id): array;
 
     /**
